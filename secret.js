e701b6b8a19b42104c6250d4a48ff9739f848ff5
LGYang <18500921794@163.com>
2022-12-09 16:34:29 +0800

    'update'

---
+++ secret.js
@@ -0,0 +1,12 @@
+module.exports = {
+    mnemonic: "",
+    dev: "",
+    deployer: "",
+    oklinkKey: "",
+    etherscanKey: '',
+    bscscanKey: '',
+    hecoinfoKey: '',
+    ftmscanKey: '',
+    polygonscanKey: '',
+    arbiscanKey: '',
+};
