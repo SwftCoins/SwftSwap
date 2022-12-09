75a1eb13633e48c00db8cad9ca3348d77b34a696
LGYang <18500921794@163.com>
2022-12-09 16:48:20 +0800

    'update'

--- migrations/_0_deploy_SwftSwap.js
+++
@@ -1,8 +0,0 @@
-const SwftSwap = artifacts.require('SwftSwap');
-const ehters = require('ethers');
-
-module.exports = function (deployer, network, accounts) {
-	return deployer.deploy(SwftSwap).then(res => {
-		console.log('accounts[0]: ' + accounts[0]);
-	});
-};
